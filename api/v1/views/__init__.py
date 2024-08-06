51785311a2c1a363091a272b6fce24fd32d38423
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-08-06 20:03:57 +0300

    State

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Importing views after creating the Blueprint
 from api.v1.views.index import *
+from api.v1.views.states import *
