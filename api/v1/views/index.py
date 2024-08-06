04bc9e8d707b0b880e9cb578f36d44b0384fe9c5
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-08-06 19:22:36 +0300

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,26 @@ Creates the route status endpoint
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """
+    Retrieves the number of each object by type.
+    """
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
